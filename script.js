a99851bdef047a0e430e5f3f1d4e7e7155d197ee
Dilbart436 <dilbartb35@gmail.com>
2026-03-21 12:44:39 +0530

    Refactor addTask to arrow function and improve DOM handling

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
-function addTask() {
+const addTask = () => {
     if (inputBox.value === '') {
         alert("You must write something!");
-    }
-    else {
-        let li = document.createElement("li");
+    } else {
+        const li = document.createElement('li');
         li.textContent = inputBox.value;
         listContainer.appendChild(li);
-
-        let span = document.createElement("span");
+        const span = document.createElement('span');
         span.textContent = "\u00d7";
         li.appendChild(span);
     }
-
     inputBox.value = "";
-}
+    saveData();
+};
 
 
-listContainer.addEventListener('click', (e) => {
-    if (e.target.matches("li")) {
-        e.target.classList.toggle("checked");
-    }
-    else if (e.target.matches("span")) {
-        e.target.parentElement.remove();
+listContainer.addEventListener('click', (event) => {
+    if (event.target.matches('li')) {
+        event.target.classList.toggle("checked");
+        saveData();
+    } else if (event.target.matches('span')) {
+        event.target.parentElement.remove();
+        saveData();
     }
 }, false);
+
+
+const saveData = () => {
+    localStorage.setItem("data", listContainer.innerHTML);
+};
+
+const showTask = () => {
+    listContainer.innerHTML = localStorage.getItem("data");
+};
+
+showTask();
